dcff7acbbdb6423a9185c8516c922936b1e621ee
denver <dbabiichuk@yandex.ru>
2022-07-21 17:29:25 +0200

    bug fix

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 from werkzeug.security import check_password_hash, generate_password_hash
 
+
 app = Flask(__name__)
 app.secret_key = 'bdbabdbatestsecret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
